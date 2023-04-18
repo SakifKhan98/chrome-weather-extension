c626d0bb6566f58937a1521d322415fe1fa4308d
SakifKhan98 <sakifkhan98@gmail.com>
2023-04-19 03:19:00 +0600

    Added search city feature with delete city feature

--- src/popup/WeatherCard/WeatherCard.tsx
+++ src/popup/WeatherCard/WeatherCard.tsx
@@ -1,14 +1,29 @@
 import React, { useEffect, useState } from 'react'
-import { Box, Card, CardContent, Typography } from '@material-ui/core'
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from '@material-ui/core'
 import { fetchOpenWeatherData, OpenWeatherData } from '../../utils/api'
 
-const WeatherCardContainer: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
+const WeatherCardContainer: React.FC<{
+  children: React.ReactNode
+  onDelete?: () => void
+}> = ({ children, onDelete }) => {
   return (
     <Box mx={'4px'} my={'16px'}>
       <Card>
         <CardContent> {children} </CardContent>
+        <CardActions>
+          {onDelete && (
+            <Button color='secondary' onClick={onDelete}>
+              Delete
+            </Button>
+          )}
+        </CardActions>
       </Card>
     </Box>
   )
@@ -16,7 +31,10 @@ const WeatherCardContainer: React.FC<{ children: React.ReactNode }> = ({
 
 type WeatherCardState = 'loading' | 'error' | 'ready'
 
-const WeatherCard: React.FC<{ city: string }> = ({ city }) => {
+const WeatherCard: React.FC<{ city: string; onDelete?: () => void }> = ({
+  city,
+  onDelete,
+}) => {
   const [weatherData, setWeatherData] = useState<OpenWeatherData | null>(null)
   const [cardState, setCardState] = useState<WeatherCardState>('loading')
 
@@ -34,7 +52,7 @@ const WeatherCard: React.FC<{ city: string }> = ({ city }) => {
   }, [city])
   if (cardState == 'loading' || cardState == 'error') {
     return (
-      <WeatherCardContainer>
+      <WeatherCardContainer onDelete={onDelete}>
         <Typography variant='body1'>
           {cardState == 'loading'
             ? 'Loading...'
@@ -44,7 +62,7 @@ const WeatherCard: React.FC<{ city: string }> = ({ city }) => {
     )
   }
   return (
-    <WeatherCardContainer>
+    <WeatherCardContainer onDelete={onDelete}>
       <Typography variant='h5'>{weatherData.name}</Typography>
       <Typography variant='body1'>
         Temperature: {Math.round(weatherData.main.temp)} °C
--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,16 +1,60 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import 'fontsource-roboto'
 import './popup.css'
 import WeatherCard from './WeatherCard/WeatherCard'
+import { Box, Grid, InputBase, IconButton, Paper } from '@material-ui/core'
+import { Add as AddIcon } from '@material-ui/icons'
 
 const App: React.FC<{}> = () => {
+  const [cities, setCities] = useState<string[]>(['dhaka', 'doha', 'Errorr'])
+  const [cityInput, setCityInput] = useState<string>('')
+
+  const handleCityAdd = () => {
+    if (cityInput === '') return
+
+    setCities([...cities, cityInput])
+    setCityInput('')
+  }
+
+  const handleCityDelete = (index: number) => {
+    cities.splice(index, 1)
+    setCities([...cities])
+  }
+
+  console.log(cityInput)
   return (
-    <div>
-      <WeatherCard city='dhaka' />
-      <WeatherCard city='delhi' />
-      <WeatherCard city='Errorr' />
-    </div>
+    <Box mx={'8px'} my={'16px'}>
+      <Grid container>
+        <Grid item>
+          <Paper>
+            <Box px={'15px'} py={'5px'}>
+              <InputBase
+                placeholder='Add a city'
+                value={cityInput}
+                onChange={(event) => {
+                  setCityInput(event.target.value)
+                }}
+              />
+              <IconButton onClick={handleCityAdd}>
+                <AddIcon />
+              </IconButton>
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>
+
+      {cities.map((city, index) => (
+        <WeatherCard
+          city={city}
+          key={index}
+          onDelete={() => {
+            handleCityDelete(index)
+          }}
+        />
+      ))}
+      <Box height={'16px'} />
+    </Box>
   )
 }
 
